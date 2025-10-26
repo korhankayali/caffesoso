2dc8e7cbdba1a56d615e5ab688f34c4da6c0a980
mevlutgeredeli <40805426+mevlut-geredeli@users.noreply.github.com>
2025-10-26 15:57:40 +0300

    Update: logo2, opaque navbar, Inter font, mobile responsive

---
+++ force-deploy.bat
@@ -0,0 +1,7 @@
+@echo off
+cd /d "C:\Users\tylerdurden\Desktop\SoftVerse\Project\Soso"
+git add -A
+git commit -m "Update: logo2, opaque navbar, Inter font, mobile responsive, footer navigation"
+git push origin main --force
+echo Deployment completed!
+pause
